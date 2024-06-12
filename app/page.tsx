06d65b28f533778b5f928d5172644a06dee8a419
David A. Lozano <void9david@gmail.com>
2024-06-12 17:11:00 -0600

    part 6 - hosting and vercel postgresdb in .env

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <div
             className={styles.shape}
           />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
